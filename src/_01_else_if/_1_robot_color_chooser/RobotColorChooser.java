56d961b74a88700e2f6c3b2b3ba5b4d48027dd3c
student <student@ip-172-31-48-14.us-west-2.compute.internal>
2021-12-13 19:59:31 -0800

    RobotColorChoser done CrazyzCatLady in progress.

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -16,7 +16,7 @@ public class RobotColorChooser {
 	    //2. Make the robot draw a shape (this will take more than one line of code)
 		bob.setSpeed(15);	
 		int loop = 0;
-		while(loop<4) {
+		while(loop<8) {
 //		bob.move(100);
 //		bob.turn(90);
 		String question;
@@ -24,25 +24,25 @@ public class RobotColorChooser {
 		//3. Set the pen width to 10
 		bob.setPenWidth(10);
 		//4. Ask the user what color pen they would like the robot to draw with
-		JOptionPane.showInputDialog(null, "what color pen would you like to use?");
+		question= JOptionPane.showInputDialog(null, "what color pen would you like to use?");
 		//5. Use an if/else statement to set the pen color that the user requested
-		if ("red" != null) {
+		if (question .equalsIgnoreCase ("red")) {
 			bob.setPenColor(Color.RED);
 			}
-		else if ("blue" != null) {
+		else if (question .equalsIgnoreCase ("blue")) {
 			bob.setPenColor(Color.blue);
 			}
-		else if ("yellow" != null) {
+		else if (question .equalsIgnoreCase ("yellow")) {
 			bob.setPenColor(Color.yellow);
 		}
         //6. If the user doesn't enter anything, choose a random color
-		else if ("" != null) {
+		else if (question .equalsIgnoreCase ("")) {
 			bob.setPenColor(Color.BLACK);
 		}
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		bob.penDown();
 		bob.move(100);
-		bob.turn(90);
+		bob.turn(45);
 		loop+=1;
 		}
 
