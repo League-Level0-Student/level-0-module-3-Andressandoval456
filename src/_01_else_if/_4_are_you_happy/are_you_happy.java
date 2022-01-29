49219c6012d972439b45ad23eb7f794d0c0bfa64
student <student@ip-172-31-57-171.us-west-2.compute.internal>
2022-01-28 18:29:29 -0800

    are_you_happy done if_elseif_story in progress.

--- src/_01_else_if/_4_are_you_happy/are_you_happy.java
+++ src/_01_else_if/_4_are_you_happy/are_you_happy.java
@@ -6,8 +6,20 @@ public class are_you_happy {
 	public static void main(String[] args) {
 		String question = JOptionPane.showInputDialog(null, "Are You Happy?");
 			if (question .equals("yes")) {
-				
+				JOptionPane.showMessageDialog(null, "Keep doing what yuo are doing");
+			}
+			else if (question .equals("no")) {
+				String doyou = JOptionPane.showInputDialog(null, "D You hant to be Happy?");
+				if (doyou .equals("yes")){
+					JOptionPane.showMessageDialog(null, "Change somthing");
+				}
+				else if (doyou .equals ("no")) {
+					JOptionPane.showMessageDialog(null, "Keep doing what yuo are doing");
+				}
+			}
+
+			
 			}
 		
 	}
-}
+
---
+++ src/_01_else_if/_5_own_adventure/If_elseif_story.java
@@ -0,0 +1,9 @@
+package _01_else_if._5_own_adventure;
+
+import javax.swing.JOptionPane;
+
+public class If_elseif_story {
+public static void main(String[] args) {
+	JOptionPane.showInputDialog("");
+}
+}
