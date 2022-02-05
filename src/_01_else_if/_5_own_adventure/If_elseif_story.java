f8a907298c972a0f7fd34c807d9538cb7a8bb990
student <student@ip-172-31-53-115.us-west-2.compute.internal>
2022-02-04 18:23:45 -0800

    if_else_story in progress.

--- src/_01_else_if/_5_own_adventure/If_elseif_story.java
+++ src/_01_else_if/_5_own_adventure/If_elseif_story.java
@@ -4,6 +4,28 @@ import javax.swing.JOptionPane;
 
 public class If_elseif_story {
 public static void main(String[] args) {
-	JOptionPane.showInputDialog("");
+	String qone;
+	qone = JOptionPane.showInputDialog("Red or Black");
+		if (qone .equals("red")){
+//			System.out.println("red");
+			JOptionPane.showMessageDialog(null, "You chose the red car");
+			String qrone = JOptionPane.showInputDialog("Do you go to your house or the DMV");
+				if (qrone .equals("home")) {
+					System.out.println("home");
+				}
+				else if (qrone .equals("DMV")) {
+					System.out.println("DMV");
+				}		if_
 }
+		else if (qone .equals("black")) {
+//			System.out.println("black");
+			JOptionPane.showMessageDialog(null, "You chose the black car");
+			String qbone = JOptionPane.showInputDialog("Y");
+			
+		}
+	
 }
+
+}
+
+
