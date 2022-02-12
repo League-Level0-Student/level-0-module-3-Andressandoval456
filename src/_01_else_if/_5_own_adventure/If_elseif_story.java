386f1b84af142582dcd9dc55ceb5f4aff8f4480c
student <student@ip-172-31-62-223.us-west-2.compute.internal>
2022-02-11 18:27:28 -0800

    if_else_story in progress.

--- src/_01_else_if/_5_own_adventure/If_elseif_story.java
+++ src/_01_else_if/_5_own_adventure/If_elseif_story.java
@@ -15,17 +15,36 @@ public static void main(String[] args) {
 				}
 				else if (qrone .equals("DMV")) {
 					System.out.println("DMV");
-				}		if_
+				}		
 }
 		else if (qone .equals("black")) {
 //			System.out.println("black");
 			JOptionPane.showMessageDialog(null, "You chose the black car");
-			String qbone = JOptionPane.showInputDialog("Y");
-			
+			String qbone = JOptionPane.showInputDialog("Do you go to your house to get food");
+			if (qbone .equals("house")) {
+				String qbtwo = JOptionPane.showInputDialog(null, "You get to your house, do you watch tv or make yourself something to eat?");
+				if (condition) {
+					
+				} else {
+
+				}
+			}
+			else if (qbone .equals("food")) {
+				String qbtree = JOptionPane.showInputDialog(null, "Do you get pizza , hamburgers or tacos");
+				if (qbtree .equals ("pizza")) {
+					JOptionPane.showMessageDialog(null, "You got a pizza");
+				}
+				else if (qbtree .equals("hamburgers")) {
+					JOptionPane.showMessageDialog(null, "You got a hamburger");
+					
+				}
+				else if (qbtree .equals("tacos")) {
+					JOptionPane.showMessageDialog(null, "You got tacos");
+				}
+			}
 		}
 	
 }
 
 }
-
 
